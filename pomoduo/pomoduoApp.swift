d06baf24b00d1691fa8148f53e9baf0fe0134cb7
vansh-09 <vanshkjain09@gmail.com>
2025-09-16 02:13:37 +0530

    Initial Commit

---
+++ pomoduo/pomoduoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomoduoApp.swift
+//  pomoduo
+//
+//  Created by Vansh Jain on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct pomoduoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pomoduoTests/pomoduoTests.swift
@@ -0,0 +1,16 @@
+//
+//  pomoduoTests.swift
+//  pomoduoTests
+//
+//  Created by Vansh Jain on 16/09/25.
+//
+
+import Testing
+
+struct pomoduoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
